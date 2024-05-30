9bb82d3a93a208b23108609de2877ba7b70ee448
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-30 15:19:22 +0200

    [Completed] Milestone 3&4 -> New Vue project created + axios installed + callAPI method for take data and print it in my console-log

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script >
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      base_api_url: 'http://127.0.0.1:8000',
+      base_projects_url: '/api/projects',
+      projects: [],
+      //loading: true
+    }
+  },
+  methods: {
+    callAPI(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+          //this.projects = response.data.projects
+          //this.loading = false
+        })
+        .catch(err => {
+          console.error(err);
+        })
+    }
+  },
+
+  mounted() {
+    let url = this.base_api_url + this.base_projects_url
+    this.callAPI(url);
+  }
+}
 
 </script>
 
 <template>
-Hello Portofolio!
+  Hello Portofolio!
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
