06322418f2332d4316d5d11cae419425977d8f68
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-30 17:08:34 +0200

    [Completed] [Bonus] Pagination managed + some style with BS

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,21 @@ export default {
     }
   },
   methods: {
+
+    /**Mi permette di effettuare una chiama API al mio server locale con il parametro che passo in pagina
+     * 
+     * @param {string} page 
+     */
+    goTo(page) {
+      //console.log(page);
+      //Sfrutto callAPI
+      this.callAPI(page)
+    },
+
+    /**Mi permette di effettuare una chiama API al mio server locale
+ * 
+ * @param {string} url 
+ */
     callAPI(url) {
       axios
         .get(url)
@@ -34,6 +49,7 @@ export default {
   },
 
   mounted() {
+    //Chiamo la funzione nel Mounted per avere già dati in pagina
     let url = this.base_api_url + this.base_projects_url
     this.callAPI(url);
   }
@@ -42,17 +58,64 @@ export default {
 </script>
 
 <template>
-  <main>
-    <div class="container">
-      <div class="row">
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-primary mb-3">
+      <div class="container">
+        <a class="navbar-brand fw-bold" href="#">My portfolio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+          aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarID">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
 
-        <ProjectCard :project="project" v-for="project in projects.data"></ProjectCard>
-
+          </div>
+        </div>
       </div>
+    </nav>
+  </header>
+
+  <main>
+    <h2 class="text-center m-5">Welcome, let's see my projects</h2>
+    <div class="container">
+      <div class="row g-4">
+
+        <!--Ciclo all'interno del mio array, ho bisogno del .data perché il resto del mio array associativo mi da altre info-->
+        <ProjectCard :project="project" v-for="project in projects.data"></ProjectCard>
+      </div>
+      <nav aria-label="Page navigation">
+        <ul class="pagination    ">
+
+          <!--Viaggio all'interno delle pagine richiamando il metodo goTo che a sua volta richiama il metodo callApi-->
+          <!--Prev-->
+          <li class="page-item" v-show="projects.prev_page_url" @click="goTo(projects.prev_page_url)">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+
+          <!--Next-->
+          <li class="page-item" v-show="projects.next_page_url" @click="goTo(projects.next_page_url)">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
     </div>
 
 
   </main>
+
+  <footer>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div class="container-fluid d-flex justify-content-center">
+        <a class="navbar-brand" href="#">Copyright 2024 - Giacomo Marongiu @119</a>
+      </div>
+    </nav>
+  </footer>
 </template>
 
 <style scoped></style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,13 +15,25 @@ export default {
 
 <template>
 
-    <div class="col-4">
+    <div class="col-6">
         <div class="card">
-            <!--             <img class="card-img-top" src="" alt="Title" />
- -->
-            <div class="card-body">
-                <h4 class="card-title">{{project.title}}</h4>
-                <p class="card-text">{{ project.description }}</p>
+            <template v-if="project.img.startsWith('uploads')">
+                <img class="card-img-top" :src="base_api_url + '/storage/' + project.img" alt="">
+            </template>
+            <template v-else>
+                <img class="card-img-top" :src="project.img" alt="">
+            </template>
+
+            <div class="card-body d-flex flex-column ">
+                <h4 class="card-title h-25">{{ project.title }}</h4>
+                <p class="card-text h-50 overflow-hidden mt-5">{{ project.description }}</p>
+                <div class="card-body h-25">
+                    <a :href="project.github_link" class="btn btn-primary mb-2">View it on GitHub</a>
+                    <p class="card-text text-start fw-bold m-2">Project start: <span class="fw-normal">{{ project.start_date
+                            }}</span></p>
+                    <p class="card-text text-start fw-bold m-2">Project end: <span class="fw-normal">{{ project.end_date
+                            }}</span></p>
+                </div>
             </div>
         </div>
     </div>
