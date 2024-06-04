3d99cb8e2396d1ef51415dc9902b526d21242cf1
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-06-04 15:30:16 +0200

    Add some layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,12 @@
 <script>
 export default {
-name: "AppHeader",
+    name: "AppHeader",
 }
 </script>
 
 <template>
     <div class="links d-flex justify-content-end">
+        <div class="ball ibm-plex-mono-bold">Booleaner</div>
         <nav class="navbar ">
             <form class="container-fluid justify-content-end">
                 <RouterLink class="btn border-0 btn-sm btn-outline-danger text-decoration-none fw-bold"
@@ -25,4 +26,22 @@ name: "AppHeader",
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.ball {
+    height: 250px;
+    width: 250px;
+    border: 1rem solid rgba(196, 48, 48, 0.722);
+    border-radius: 50%;
+    position: absolute;
+    left: 10%;
+    top: 10%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: rgba(196, 48, 48, 0.722);
+    font-size: 70px;
+    transform: rotate(320deg);
+    z-index: 1;
+
+}
+</style>
